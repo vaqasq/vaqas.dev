15e3d906d6685c2bd3c719bae2acc437716de8aa
HealingAura32 <vqudus@gmail.com>
2026-05-16 23:46:50 -0400

    image worksgit add .

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module personal-site
 
 go 1.26.2
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,17 +1,62 @@
 package main
 
 import (
+	"encoding/json"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
-/*
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world!")
+func fetchImage(apiKey string) string {
+
+	// define struct to get the field you want
+	type Response struct {
+		ImageURL string `json:"url"`
+	}
+
+	//use api
+	reponse, err := http.Get("https://api.nasa.gov/planetary/apod?api_key=" + apiKey)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer reponse.Body.Close()
+
+	var resp Response
+
+	if err := json.NewDecoder(reponse.Body).Decode(&resp); err != nil {
+		log.Fatal(err)
+	}
+
+	// resp.ImageURL is the image URL
+	return resp.ImageURL
+
 }
-*/
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	apiKey := os.Getenv("NASA_API_KEY")
+	imageURL := fetchImage(apiKey)
+
+	tmpl, err := template.ParseFiles("static-files/index.html")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tmpl.Execute(w, imageURL)
+
+}
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./static-files")))
+
+	godotenv.Load()
+
+	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
